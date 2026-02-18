897b17bafc752502fff8e8fd90666a6939cc75b2
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2026-02-18 01:37:18 +0100

    Don't return null but throw exceptions when something goes wrong

--- GlyfiBot/Commands/TypstCommand.cs
+++ GlyfiBot/Commands/TypstCommand.cs
@@ -3,6 +3,7 @@ using NetCord;
 using NetCord.Rest;
 using NetCord.Services.ApplicationCommands;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using static GlyfiBot.Utils;
@@ -22,11 +23,8 @@ public partial class TypstCommand : ApplicationCommandModule<SlashCommandContext
 		await RespondAsync(InteractionCallback.DeferredMessage(MessageFlags.Ephemeral));
 		using HttpClient client = new();
 
-		string? typstExe = await SetupTypst(client);
-		if (typstExe is null) return; //no need to message, that was already done in the function
-
-		string? scriptPath = await SetupScript(client);
-		if (scriptPath is null) return; //no need to message, that was already done in the function
+		string typstExe = await SetupTypst(client);
+		string scriptPath = await SetupScript(client);
 
 		Process typstCmd = new() {StartInfo = new ProcessStartInfo(typstExe, ["compile", scriptPath]) {RedirectStandardOutput = true, RedirectStandardError = true}};
 		typstCmd.Start();
@@ -42,14 +40,9 @@ public partial class TypstCommand : ApplicationCommandModule<SlashCommandContext
 
 #region Setup Script
 
-	private async Task<string?> SetupScript(HttpClient client)
+	private async Task<string> SetupScript(HttpClient client)
 	{
-		string? latestCommitHash = await GetLatestCommitHash(client);
-		if (latestCommitHash is null)
-		{
-			await Context.ModifyEphemeralResponseAsync("Failed to retrieve the latest commit hash of the Typst script!");
-			return null;
-		}
+		string latestCommitHash = await GetLatestCommitHash(client);
 
 		string scriptDir = Path.Join(Program.TYPST_SCRIPT_DIR, $"{SCRIPTS_REPO_NAME}-{latestCommitHash}");
 		if (!Directory.Exists(scriptDir))
@@ -69,19 +62,13 @@ public partial class TypstCommand : ApplicationCommandModule<SlashCommandContext
 		}
 
 		string scriptPath = Path.Join(scriptDir, "main.typ");
-		if (!File.Exists(scriptPath))
-		{
-			await Context.ModifyEphemeralResponseAsync("Could not find `main.typ` in the script folder!");
-			return null;
-		}
-
-		return scriptPath;
+		return File.Exists(scriptPath) ? scriptPath : throw new FileNotFoundException("Could not find `main.typ` in the script folder!");
 	}
 
 	[GeneratedRegex("""<meta\s+property=(["'])og:url\1\s+content=(["']).+/commit/(.+?)/?\2\s*/>""")]
 	private static partial Regex HashRegex();
 
-	private static async Task<string?> GetLatestCommitHash(HttpClient client)
+	private static async Task<string> GetLatestCommitHash(HttpClient client)
 	{
 		await using Stream networkStream = await client.GetStreamAsync($"https://github.com/glyphs-fi/{SCRIPTS_REPO_NAME}/commit/main");
 		using StreamReader streamReader = new(networkStream);
@@ -96,21 +83,16 @@ public partial class TypstCommand : ApplicationCommandModule<SlashCommandContext
 			}
 		}
 
-		return null;
+		throw new Exception("Failed to retrieve the latest commit hash of the Typst script!");
 	}
 
 #endregion
 
 #region Setup Typst
 
-	private async Task<string?> SetupTypst(HttpClient client)
+	private async Task<string> SetupTypst(HttpClient client)
 	{
-		string? typstDownloadURL = GetTypstDownloadURLForPlatform();
-		if (typstDownloadURL is null)
-		{
-			await Context.ModifyEphemeralResponseAsync("The server isn't running on a platform that this bot supports, so Typst cannot be installed...");
-			return null;
-		}
+		string typstDownloadURL = GetTypstDownloadURLForPlatform();
 
 		string typstExeVersionDir = Path.Join(Program.TYPST_EXE_DIR, TYPST_VERSION);
 		if (!Directory.Exists(typstExeVersionDir))
@@ -130,13 +112,7 @@ public partial class TypstCommand : ApplicationCommandModule<SlashCommandContext
 		}
 
 		string? exeLocation = FindExe(typstExeVersionDir, "typst");
-		if (exeLocation is null)
-		{
-			await Context.ModifyEphemeralResponseAsync("Could not find a Typst executable in the unpacked archive!");
-			return null;
-		}
-
-		return exeLocation;
+		return exeLocation ?? throw new FileNotFoundException("Could not find a Typst executable in the unpacked archive!");
 	}
 
 	private const string URL = $"https://github.com/typst/typst/releases/download/{TYPST_VERSION}";
@@ -144,7 +120,8 @@ public partial class TypstCommand : ApplicationCommandModule<SlashCommandContext
 	private const string URL_LINUX_ARM64 = $"{URL}/typst-aarch64-unknown-linux-musl.tar.xz";
 	private const string URL_WIN_X64 = $"{URL}/typst-x86_64-pc-windows-msvc.zip";
 
-	private static string? GetTypstDownloadURLForPlatform()
+	[SuppressMessage("ReSharper", "SwitchExpressionHandlesSomeKnownEnumValuesWithExceptionInDefault")]
+	private static string GetTypstDownloadURLForPlatform()
 	{
 		if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
 		{
@@ -152,7 +129,7 @@ public partial class TypstCommand : ApplicationCommandModule<SlashCommandContext
 			{
 				Architecture.X64 => URL_LINUX_X64,
 				Architecture.Arm64 => URL_LINUX_ARM64,
-				_ => null,
+				_ => throw new PlatformNotSupportedException("The bot is running on a server that is not of an Architecture for Linux that this bot supports, so Typst cannot be installed!"),
 			};
 		}
 
@@ -161,11 +138,11 @@ public partial class TypstCommand : ApplicationCommandModule<SlashCommandContext
 			return RuntimeInformation.OSArchitecture switch
 			{
 				Architecture.X64 => URL_WIN_X64,
-				_ => null,
+				_ => throw new PlatformNotSupportedException("The bot is running on a server that is not of an Architecture for Windows that this bot supports, so Typst cannot be installed"),
 			};
 		}
 
-		return null;
+		throw new PlatformNotSupportedException("The bot is running on a server that is not of an Operating System that this bot supports, so Typst cannot be installed!");
 	}
 
 #endregion
